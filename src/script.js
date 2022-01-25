c653fffbfc7bef24e9b5d8eba509ba1d2c006726
Troy_R <troyrichards28@gmail.com>
2022-01-25 17:09:56 -0500

    Created Component in JS

--- src/script.js
+++ src/script.js
@@ -1,20 +1,47 @@
-const currentTime = document.querySelector('.current-time')
-const prevTime = document.querySelector('.prev-time')
 const dailyBtn = document.querySelector('.daily')
 const weeklyBtn = document.querySelector('.weekly')
-const monthyBtn = document.querySelector('.monthy')
+const monthlyBtn = document.querySelector('.monthly')
+const contentWrapper = document.querySelector('.container')
 
 
-const getData = async function () {
+const getData = async function() {
     const res = await fetch('data.json');
     const data = await res.json();
-    /* console.log(data) */
+    console.log(data);
+    loadCards(data)
 }
 
 getData()
 
-dailyBtn.addEventListener("click", dailyStats)
 
-function dailyStats() {
-   currentTime.innerHTML = "" 
+const loadCards = function (array) {
+    array.forEach(card =>{
+      const section = document.createElement('section')
+      section.className = `card card-back ${card.title.split(' ').join('-').toLowerCase()}`
+      section.innerHTML = `
+        <!-- ${card.title} -->
+
+        <div class="card-bottom">
+        <header class="title">
+            <h2>
+              ${card.title}
+            </h2>
+            <i class="dots">
+            </i>
+        </header>
+      
+      <article class="stats">
+          <p class="current-time">
+            ${card.timeframes.daily.current}hrs
+          </p>
+          <p class="prev-time">
+            Previous - ${card.timeframes.daily.previous} hrs
+          </p>
+      </article>
+      </div>
+    `
+    contentWrapper.append(section)
+    })
 }
+
+dailyBtn.addEventListener('click' )
