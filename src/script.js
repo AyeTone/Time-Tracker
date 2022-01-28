a807069f679c5b6b4967226dc4b0d2d6310f8fbc
Troy_R <troyrichards28@gmail.com>
2022-01-28 10:51:36 -0500

    Altered JS

--- src/script.js
+++ src/script.js
@@ -3,73 +3,53 @@ const weeklyBtn = document.querySelector('.weekly')
 const monthlyBtn = document.querySelector('.monthly')
 const contentWrapper = document.querySelector('.container')
 
+let newData;
 
 const getData = async function() {
-    const res = await fetch('data.json');
-    const data = await res.json();
-    console.log(data);
-    loadCards(data)
+  const res = await fetch("data.json");
+  const data = await res.json();
+  newData = data
+  loadCards()
 }
 
 getData()
 
+function loadCards() {
+  newData.map((data) => {
+    const cardBack = document.createElement('div')
+    const cardBottom = document.createElement('div')
+    const cardHeader = document.createElement('div')
+    const cardTitle = document.createElement('h2')
+    const cardDots = document.createElement('i')
+    const cardStats = document.createElement('div')
+    const currentTime = document.createElement('p')
+    const prevTime = document.createElement('p')
+    
+    const cardName = data.title.split(' ').join('-').toLowerCase()
 
-const loadCards = function (array) {
-    array.forEach(card =>{
-      const div = document.createElement('div')
-      div.className = `card card-back ${card.title.split(' ').join('-').toLowerCase()}`
-      div.innerHTML = `
-        <!-- ${card.title} -->
 
-        <div class="card-bottom">
-        <div class="title">
-            <h2>
-              ${card.title}
-            </h2>
-            <i class="dots">
-            </i>
-        </div>
-      
-      <article class="stats">
-          <p class="current-time">
-            ${card.timeframes.daily.current}hrs
-          </p>
-          <p class="prev-time">
-            Previus - ${card.timeframes.daily.previous} hrs
-          </p>
-      </article>
-      </div>
-    `
-    contentWrapper.append(div)
-    })
+    cardBack.className = `card card-back ${cardName}`
+    cardBottom.className = 'card-bottom'
+    cardHeader.className = 'card-header'
+    cardDots.className = 'dots'
+    cardStats.className = 'stats'
+    currentTime.className = 'current-time'
+    prevTime.className = 'prev-time'
+
+
+    cardTitle.innerHTML = data.title 
+    currentTime.innerHTML = `${data.timeframes.daily.current}hrs`
+    prevTime.innerHTML = `Last Week - ${data.timeframes.daily.previous}hrs`
+
+    contentWrapper.append(cardBack)
+    cardBack.append(cardBottom)
+    cardBottom.append(cardHeader)
+    cardBottom.append(cardStats)
+    cardHeader.append(cardTitle, cardDots)
+    cardStats.append(currentTime, prevTime)
+
+
+  })
 }
 
-weeklyBtn.addEventListener('click', function (array) {
-  array.forEach(card =>{
-    const div = document.createElement('div')
-    div.className = `card card-back ${card.title.split(' ').join('-').toLowerCase()}`
-    div.innerHTML = `
-      <!-- ${card.title} -->
 
-      <div class="card-bottom">
-      <div class="title">
-          <h2>
-            ${card.title}
-          </h2>
-          <i class="dots">
-          </i>
-      </div>
-    
-    <article class="stats">
-        <p class="current-time">
-          ${card.timeframes.weekly.current}hrs
-        </p>
-        <p class="prev-time">
-          Previous - ${card.timeframes.weekly.previous} hrs
-        </p>
-    </article>
-    </div>
-  `
-  contentWrapper.append(div)
-  })
-})
